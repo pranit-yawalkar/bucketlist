8918710dd27af4f239fb65528be8df5fe9ebc2a5
Pranit Yawalkar <pranityawalkar10@gmail.com>
2021-08-21 23:14:12 +0530

    settings updated

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os.path
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!u0-yh7s^^(=6!91%6)ccd-_ii%&ki-na8laxv(&($+sb__j@#'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG")
 
 ALLOWED_HOSTS = []
 
